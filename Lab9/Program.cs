24560b37f508d32f6595da8fc352eb2dcb1b7f0f
HinaChleck <hinachleck@gmail.com>
2021-11-29 23:02:40 +0500

    Добавьте файлы проекта.

---
+++ Lab9/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        //1.    Смоделируйте работу простого калькулятора. Программа должна запрашивать 2 числа, а затем – код операции
+        //(например, 1 – сложение, 2 – вычитание, 3 – произведение, 4 – частное). После этого на консоль выводится ответ.
+        //Используйте обработку исключений для защиты от ввода некорректных данных.
+        {
+
+            Console.WriteLine("КАЛЬКУЛЯТОР");
+            
+            try
+            {
+                Console.Write("Введите первое число: ");
+                float x = Convert.ToSingle(Console.ReadLine());
+                Console.Write("Введите второе число: ");
+                float y = Convert.ToSingle(Console.ReadLine());
+
+                Console.Write("Введите математическую операцию (+ , - , / ,  *): ");
+                string symbol = Console.ReadLine();
+                switch (symbol)
+                {
+                    case "+":
+                        {
+                            Console.WriteLine("{0} {2} {1} = {3}", x, y, symbol, x + y);
+                            break;
+                        }
+                    case "-":
+                        {
+                            Console.WriteLine("{0} {2} {1} = {3}", x, y, symbol, x - y);
+                            break;
+                        }
+                    case "*":
+                        {
+                            Console.WriteLine("{0} {2} {1} = {3}", x, y, symbol, x * y);
+                            break;
+                        }
+                    case "/":
+                        {
+                            if (y==0)
+                            {
+                               throw new Exception("Деление на 0 не предусмотрено");
+                            }
+                            Console.WriteLine("{0} {2} {1} = {3}", x, y, symbol, x / y);
+                            break;
+                        }
+                    default:
+                        throw new Exception("Введена некорретная операция");
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Ошибка: {0}", ex.Message);
+ 
+            }
+            
+            
+            
+            /*finally
+            {
+                try
+                {
+                    Console.Write("Введите математическую операцию (+ , - , / ,  *)");
+                    string symbol = Convert.ToString(Console.ReadLine());
+                    switch (symbol)
+                    {
+                        case "+":
+                            {
+                                Console.WriteLine("{0} {2} {1} = {3}", x, y, symbol, x + y);
+                                break;
+                            }
+                        case "-":
+                            {
+                                Console.WriteLine("{0} {2} {1} = {3}", x, y, symbol, x - y);
+                                break;
+                            }
+                        case "*":
+                            {
+                                Console.WriteLine("{0} {2} {1} = {3}", x, y, symbol, x * y);
+                                break;
+                            }
+                        case "/":
+                            {
+                                Console.WriteLine("{0} {2} {1} = {3}", x, y, symbol, x / y);
+                                break;
+                            }
+                        default:
+                            throw new Exception("Введена некорретная операция");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Ошибка: {0}", ex.Message);
+                }
+            }*/
+
+
+
+            Console.ReadKey();
+        }
+               
+        }
+        
+    }
+
